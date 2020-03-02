03faead01e19f2c0e9c6bf56a7fe8c5ab85500f1
shun0301 <xxsu007xx@icloud.com>
2020-03-02 19:49:33 +0900

    非同期通信-to-ChatSpace

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){ 
+     function buildHTML(message){
+      if ( message.image ) {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="message__top">
+              <div class="user-name">
+                ${message.user_name}
+              </div>
+              <div class="time">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="message__top">
+              <div class="user-name">
+                ${message.user_name}
+              </div>
+              <div class="time">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="messae__text">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    // $('Send').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.main').append(html);      
+       $('form')[0].reset();
+       $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});
+       $('.submit__btn').attr('disabled', false);
+     })
+     .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+})
+});
