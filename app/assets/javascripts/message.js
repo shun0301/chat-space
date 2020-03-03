cc963757ee84bd041ec43fb145cbd51eb35389ff
shun0301 <xxsu007xx@icloud.com>
2020-03-03 18:53:39 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,57 @@
 $(function(){ 
      function buildHTML(message){
-      if ( message.image ) {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="message__top">
-              <div class="user-name">
-                ${message.user_name}
-              </div>
-              <div class="time">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="message__text">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="message__top">
-              <div class="user-name">
-                ${message.user_name}
-              </div>
-              <div class="time">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="messae__text">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
+        if (message.content && message.image) {
+          var html = `<div class="message" data-message-id=` + message.id + `>` +
+            `<div class="message__top">` +
+              `<div class="user-name">` +
+                message.user_name +
+              `</div>` +
+              `<div class="time">` +
+                message.created_at +
+              `</div>` +
+            `</div>` +
+            `<div class="message__text">` +
+              `<p class="lower-message__content">` +
+                message.content +
+              `</p>` +
+              `<img src="` + message.image + `" class="lower-message__image" >` +
+            `</div>` +
+          `</div>`
+        } else if (message.content) {
+          var html = `<div class="message" data-message-id=` + message.id + `>` +
+            `<div class="message__top">` +
+              `<div class="user-name">` +
+                message.user_name +
+              `</div>` +
+              `<div class="time">` +
+                message.created_at +
+              `</div>` +
+            `</div>` +
+            `<div class="message__text">` +
+              `<p class="lower-message__content">` +
+                message.content +
+              `</p>` +
+            `</div>` +
+          `</div>`
+        } else if (message.image) {
+          var html = `<div class="message" data-message-id=` + message.id + `>` +
+            `<div class="message__top">` +
+              `<div class="user_name">` +
+                message.user_name +
+              `</div>` +
+              `<div class="time">` +
+                message.created_at +
+              `</div>` +
+            `</div>` +
+            `<div class="message__text">` +
+              `<img src="` + message.image + `" class="lower-message__image" >` +
+            `</div>` +
+          `</div>`
+        };
         return html;
       };
-    }
 $('#new_message').on('submit', function(e){
+  console.log('hello')
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -64,4 +76,30 @@ $('#new_message').on('submit', function(e){
       $('.submit__btn').attr('disabled', false);
     })
 })
+  var reloadMessages = function() {
+    console.log('hello')
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main').append(insertHTML);
+        $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
